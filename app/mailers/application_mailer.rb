9272adb0aa535960ed97ab9e317315c43710786a
Kazuhiro Kojio <kazuhiro.kojio@gmail.com>
2020-11-23 13:46:41 -0300

    :fire: Remove useless folders and files

--- app/mailers/application_mailer.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# This is a default ApplicationMailer class
-class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
-end
